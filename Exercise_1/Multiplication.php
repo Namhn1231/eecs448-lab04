d1c4a979e2fed8b601a3247a647fea4dbacb7e38
nam nguyen <n675n055@ku.edu>
2018-10-09 12:55:13 -0500

    Completed exercise 1

---
+++ Exercise_1/Multiplication.php
@@ -0,0 +1,12 @@
+ <?php    
+<table style="width:100%">
+    for($i=1; $i<=100; $i++) {
+        //echo "<tr>"
+        for($j=1; $j<=100; $j++){
+            if ()
+            echo "<td>" . i * j . "td"
+        }        
+    }
+
+</table>
+?>
